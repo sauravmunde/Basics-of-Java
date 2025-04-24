ddbfdb84c5a903c3d790e7b6f59c8de8cc72f273
sauravmunde <souravmunde2001@gmail.com>
2025-04-24 17:38:30 +0530

    first commit

---
+++ conditional_Statements.java
@@ -0,0 +1,37 @@
+public class conditional_Statements {
+    
+    public static void main(String[] args) {
+        int a =5;
+        int b =4;
+
+        if(a>b){
+
+            System.out.println("a is big");
+        }else{
+
+            System.out.println("b is big ");
+        }
+        System.out.println();
+
+        // if(){ }else if(){ }else if(){ } else{}
+
+        if(2<2){
+            System.out.println("*");
+        }else if(2 >1){
+            System.out.println("**");
+        }else if(2 == 1){
+            System.out.println("***");
+        }else{
+
+            System.out.println(" ");
+        }
+
+
+
+        System.out.println();
+    }
+
+     
+
+
+}
