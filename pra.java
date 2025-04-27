8139cccd6810e861d7d4f5bfe608921265cfc318
sauravmunde <souravmunde2001@gmail.com>
2025-04-28 00:23:11 +0530

    first commit

--- pra.java
+++ pra.java
@@ -2,39 +2,22 @@ public class pra {
 
     public static void main(String[] args) {
      
-    /* 
-    if(a/0 == b){
-        System.out.println("hello");
-    }
-    else{
-        System.out.println("hi");
-    }
-          */
-
-   
-      int a = 50,b =20;
-      
-      if( a > b){
-
-        if(a > 100)
-
-     System.out.println("ace");
-
-        if(b<100)
-
-        b = 50;
-
-      }
-
-      else if(a==b) {
-
-       System.out.println("king");
-      }
-      
-      else {
-
-    System.out.println("queen");
+     int a =80;
+     int b =365;
      
-        }
+     int c = 24 * 365;
+
+     switch(2){
+
+
+        case 0: System.out.println("hello");
+        break;
+        case 2: System.out.println((a*b) - c);
+        break;
+        case 3:System.out.println("name");
+        default: System.out.println("hiiii");
+
+
+       }
     }  
 }
