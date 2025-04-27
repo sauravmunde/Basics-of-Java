a472e0f5612bba01d877944dbae156ee3f435dce
sauravmunde <souravmunde2001@gmail.com>
2025-04-27 18:05:12 +0530

    add code

---
+++ pra.java
@@ -0,0 +1,40 @@
+public class pra {
+
+    public static void main(String[] args) {
+     
+/* 
+    if(a/0 == b){
+        System.out.println("hello");
+    }
+    else{
+        System.out.println("hi");
+    }
+          */
+
+   
+      int a = 50,b =20;
+      
+      if( a > b){
+
+        if(a > 100)
+
+     System.out.println("ace");
+
+        if(b<100)
+
+        b= 50;
+      }
+
+      else if(a==b) {
+
+       System.out.println("king");
+      }
+      
+      else {
+
+    System.out.println("queen");
+      
+      }
+    }
+    
+}
