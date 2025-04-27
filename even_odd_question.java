6c7eee3745458603fe835d42d9f58c5a37230283
sauravmunde <souravmunde2001@gmail.com>
2025-04-27 10:29:04 +0530

    changes

--- even_odd_question.java
+++ even_odd_question.java
@@ -4,8 +4,11 @@ public class even_odd_question {
 
     public static void main(String[] args) {
 
-        Scanner s = new Scanner(System.in);
-               
+      
+
+
+                Scanner s = new Scanner(System.in);
+        /*        
         int a , b;
       System.out.println("first num");
         a= s.nextInt();
@@ -20,5 +23,24 @@ public class even_odd_question {
             System.out.println("second num is greater");
         }
         System.out.println("outside if else");
-    }
+    
+     */
+
+        int a;
+        a = s.nextInt();
+
+       if( a % 2 == 0){
+
+        System.out.println(a + " is even ");
+       }
+
+       if(a % 2 == 1){
+
+        System.out.println(a + " is odd ");
+       }
+    
+
+        }
 }
+
+
