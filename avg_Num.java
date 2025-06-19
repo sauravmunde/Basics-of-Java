28d1b0fadc3501077cb40dd285df8aee44f0755d
sauravmunde <souravmunde2001@gmail.com>
2025-06-19 21:08:34 +0530

    first commit

---
+++ avg_Num.java
@@ -0,0 +1,14 @@
+public class avg_Num {
+
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 10;
+        b = 20;
+        c = 40;
+
+        int avg = (a + b + c) / 3;
+
+        System.out.println(avg);
+    }
+
+}
