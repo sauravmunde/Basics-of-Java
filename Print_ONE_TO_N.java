33cc1731222b4fa7d31bfe7fa278decbf0ca2cb9
sauravmunde <souravmunde2001@gmail.com>
2025-06-20 12:30:09 +0530

    first commit

---
+++ Print_ONE_TO_N.java
@@ -0,0 +1,13 @@
+public class Print_ONE_TO_N {
+
+    public static void main(String[] args) {
+        int i = 1;
+        int N = 50;
+
+        while (i <= 50) {
+            System.out.print(i + ",");
+            i++;
+        }
+    }
+
+}
