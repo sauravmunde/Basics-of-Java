32fed3aad7262e66bd745630965fca09b3e47239
sauravmunde <souravmunde2001@gmail.com>
2025-06-23 11:21:18 +0530

    first commit

---
+++ AreaOf_circle.java
@@ -0,0 +1,14 @@
+public class AreaOf_circle {
+
+    public static double findsum(double a, double b) {
+
+        double c = a + b;
+        return 1;
+    }
+
+    public static void main(String[] args) {
+
+        double sum = findsum(50, 20);
+        System.out.println(sum);
+    }
+}
--- Factorial.java
+++ Factorial.java
@@ -2,14 +2,24 @@ public class Factorial {
 
     public static void main(String[] args) {
 
-        int number = 6;
-        int factorial = 1;
+        /*
+         * int number = 6;
+         * int factorial = 1;
+         * 
+         * for (int i = 1; i <= number; i++) {
+         * 
+         * factorial = factorial * i;
+         * }
+         * System.out.println("factorial of " + number + " is " + factorial);
+         */
 
-        for (int i = 1; i <= number; i++) {
+        int n = 0;
+        if (n % 2 == 0) {
+            System.out.println("even");
+        } else {
 
-            factorial = factorial * i;
+            System.out.println("odd");
         }
-        System.out.println("factorial of " + number + " is " + factorial);
 
     }
 
